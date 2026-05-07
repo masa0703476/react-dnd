f2349f95d819faa75243302e3931a5c4eb7900d8
masa0703476 <skt.ani98@gmail.com>
2026-05-08 08:35:45 +0900

    不要なレンダリング抑制 |  ドロップレイヤーのサイズ修正

--- examples_ts/04-sortable/simple/src/SuppressingUnnecessaryUpdates/DragAreaWrapper.tsx
+++ examples_ts/04-sortable/simple/src/SuppressingUnnecessaryUpdates/DragAreaWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect, useState, CSSProperties } from "react";
 import { useDrop } from "react-dnd";
 import { DropResult, ItemTypes } from "./ItemTypes";
 
@@ -19,11 +19,16 @@ const DragAreaWrapper = (props: DragAreaWrapperProps) => {
   const childrenRef = useRef<HTMLDivElement>(null);
   const [childHeight, setChildHeight] = useState(0);
 
-  const [_, dropUpper] = useDrop<DragItem, DropResult>({
+  const [{ isOver: isUpperOver }, dropUpper] = useDrop<
+    DragItem,
+    DropResult,
+    { isOver: boolean }
+  >({
     accept: ItemTypes.CARD,
     collect(monitor) {
       return {
         handlerId: monitor.getHandlerId(),
+        isOver: monitor.isOver(),
       };
     },
     drop: () => {
@@ -31,11 +36,16 @@ const DragAreaWrapper = (props: DragAreaWrapperProps) => {
     },
   });
 
-  const [__, dropBottom] = useDrop<DragItem, DropResult>({
+  const [{ isOver: isBottomOver }, dropBottom] = useDrop<
+    DragItem,
+    DropResult,
+    { isOver: boolean }
+  >({
     accept: ItemTypes.CARD,
     collect(monitor) {
       return {
         handlerId: monitor.getHandlerId(),
+        isOver: monitor.isOver(),
       };
     },
     drop: () => {
@@ -45,26 +55,36 @@ const DragAreaWrapper = (props: DragAreaWrapperProps) => {
 
   useEffect(() => {
     if (childrenRef.current) {
-      setChildHeight(childrenRef.current.offsetHeight / 2);
+      setChildHeight(childrenRef.current.offsetHeight);
     }
   }, [props.children]);
 
   const showDropLayer = props.isAnyDragged && !props.isDraggedOneself;
+  const upperDropHeight = childHeight / 2 + (isUpperOver ? childHeight : 0);
+  const bottomDropHeight = childHeight / 2 + (isBottomOver ? childHeight : 0);
+  const wrapperStyle: CSSProperties = {
+    position: "relative",
+    paddingTop: isUpperOver ? `${childHeight}px` : undefined,
+    paddingBottom: isBottomOver ? `${childHeight}px` : undefined,
+  };
+
+  // 自身がドラッグ開始された場合、元あった場所は非表示
+  if (props.isDraggedOneself) {
+    return <></>;
+  }
+
   return (
-    <div
-      style={{
-        position: "relative",
-      }}
-    >
+    <div style={wrapperStyle}>
       {showDropLayer && (
         <div
           ref={dropUpper}
           style={{
             position: "absolute",
             top: 0,
+            bottom: bottomDropHeight,
             left: 0,
             right: 0,
-            height: `${childHeight}px`,
+            height: `${upperDropHeight}px`,
             pointerEvents: "auto",
             zIndex: 1,
           }}
@@ -76,10 +96,11 @@ const DragAreaWrapper = (props: DragAreaWrapperProps) => {
           ref={dropBottom}
           style={{
             position: "absolute",
+            top: upperDropHeight,
             bottom: 0,
             left: 0,
             right: 0,
-            height: `${childHeight}px`,
+            height: `${bottomDropHeight}px`,
             pointerEvents: "auto",
             zIndex: 1,
           }}
